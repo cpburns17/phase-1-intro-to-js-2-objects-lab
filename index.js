53eb4c1ccb6a54ec99a1fbe80735c0049a339a65
Connor Burns <cpburns17@gmail.com>
2023-11-01 17:36:36 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Connor Burns",
+    streetAddress: "190 s 8th st"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = employee;
+    newEmployee[key] = value;
+    return newEmployee;
+} 
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const newEmployee = employee;
+    delete newEmployee[key];
+    return newEmployee;
+}
